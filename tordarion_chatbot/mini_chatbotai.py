90d7f3c388d7cc0126a8e929d0f7bfcdda33641d
DanishSafaraz <trialzamat1@gmail.com>
2026-09-13 20:08:37 +0700

    Adding deepseek from seekai api

--- tordarion_chatbot/mini_chatbotai.py
+++ tordarion_chatbot/mini_chatbotai.py
@@ -2,14 +2,17 @@ import openai
 import anthropic
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
 api_claude = os.getenv("API_KEY_CLAUDE")
 api_chatgpt = os.getenv("API_KEY_CHATGPT")
+api_deepseek = os.getenv("API_KEY_DEEPSEK")
 
 memanggilclaude = anthropic.Anthropic(api_key=api_claude)
 memanggilchatgpt = openai.OpenAI(api_key=api_chatgpt)
+memanggildeepseek = openai.OpenAI(api_key = api_deepseek, base_url = "https://seekai.cc/v1")
 
 message = []
 
@@ -18,9 +21,10 @@ name = str(input("Masukkan nama user:"))
 print(f'Halo {name}, selamat datang di mini chatbot Torda AI by RAN Group.')
 print('''Model AI:
 1. Claude
-2. ChatGPT''')
+2. ChatGPT
+3. DeepSeek''')
 
-user = int(input("Pilih Model (1/2):"))
+user = int(input("Pilih Model (1/2/3):"))
 
 if user == 1:
     while True:
@@ -55,3 +59,24 @@ elif user == 2:
             respon = memanggilchatgpt.responses.create(model = "gpt-5.6-luna", input = message, store = False)
     
             print("AI:", respon.output_text)
+
+elif user == 3:
+    while True:
+            chat = input(f'{name}:').lower()
+    
+            if chat == "q":
+                break
+    
+            message.append({"role": "user", "content": chat})
+    
+            respon = memanggildeepseek.chat.completions.create(model = "deepseek-v4-flash", 
+                                                        messages = message,
+                                                        stream=False, 
+                                                        reasoning_effort="high", 
+                                                        extra_body={"thinking": {"type": "enabled"}})
+
+            jawab = ""
+
+            message.append({"role": "system", "content": jawab})
+    
+            print("AI:", respon.choices[0].message.content)
